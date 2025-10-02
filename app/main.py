dd50ea23b54cfb17bbe4fcf8b7529040c7b7ce42
9asdaoui <oussamaqasdaoui@gmail.com>
2025-10-02 16:12:59 +0100

    created the interface with steamlit

---
+++ app/main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+
+# Load the trained model
+model = joblib.load("models/XGBoost_Final.pkl")
+
+# Title
+st.title("Insurance Charges Estimator")
+
+# Input fields for user
+age = st.number_input("Age", min_value=0, max_value=120, value=30)
+bmi = st.number_input("BMI", min_value=0.0, max_value=50.0, value=25.0)
+region = st.selectbox("region?", ["southwest", "southeast", "northwest", "southwest"])
+children = st.number_input("Number of children", min_value=0, max_value=10, value=0)
+smoker = st.selectbox("Smoker?", ["No", "Yes"])
+sex = st.selectbox("sex?", ["female", "male"])
+
+# Predict charges when user clicks button
+if st.button("Estimate Charges"):
+    user_data = pd.DataFrame({
+    'age': [age],
+    'bmi': [bmi],
+    'children': [children],
+    'smoker': [smoker],
+    'sex': [sex],
+    'region': [region]
+    })
+    charges = np.expm1(model.predict(user_data))
+    st.success(f"Estimated charges: {charges[0]:.2f}")
